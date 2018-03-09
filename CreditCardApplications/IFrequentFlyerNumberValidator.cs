282ba457e8ffec70f1808da7dee39e3fc9b1ee1f
unknown <petrov_desislav@abv.bg>
2018-03-09 04:30:22 +0200

    projects setup and added three tests

---
+++ CreditCardApplications/IFrequentFlyerNumberValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CreditCardApplications
+{
+    public interface IFrequentFlyerNumberValidator
+    {
+        bool IsValid(string frequentFlyerNumber);
+        void IsValid(string frequentFlyerNumber, out bool isValid);
+    }
+}
